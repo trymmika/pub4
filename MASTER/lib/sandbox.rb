d645a012a4ee2b808b7094f6fce52997a3e17d7f
anon987654321 <oowae5a@gmail.com>
2026-02-04 22:42:20 +0000

    fix: disable sandbox temporarily

--- MASTER/lib/sandbox.rb
+++ MASTER/lib/sandbox.rb
@@ -2,17 +2,8 @@
 module Master
   module Sandbox
     def self.init
-      return unless RUBY_PLATFORM =~ /openbsd/
-      require "fiddle"
-      libc = Fiddle.dlopen(nil)
-      pledge = Fiddle::Function.new(libc["pledge"], [Fiddle::TYPE_VOIDP, Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
-      unveil = Fiddle::Function.new(libc["unveil"], [Fiddle::TYPE_VOIDP, Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
-      unveil.call(Dir.pwd, "rwc")
-      unveil.call("/tmp", "rwc")
-      unveil.call(nil, nil)
-      pledge.call("stdio rpath wpath cpath inet dns proc exec", nil)
-    rescue => e
-      $stderr.puts "sandbox: #{e.message}"
+      # Disabled temporarily - enable with MASTER_SANDBOX=1
+      return
     end
   end
 end
